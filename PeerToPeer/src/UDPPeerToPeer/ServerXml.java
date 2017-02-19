9041f6a97f026a46efb059cef7a2d8d950e3a550
jinyuttt <jinyuttt@sina.com>
2017-02-19 16:46:57 +0800

    UDP穿墙组织数据

---
+++ PeerToPeer/src/UDPPeerToPeer/ServerXml.java
@@ -0,0 +1,32 @@
+  
+/**    
+* 文件名：ServerXml.java    
+*    
+* 版本信息：    
+* 日期：2017年2月8日    
+* Copyright 足下 Corporation 2017     
+* 版权所有    
+*    
+*/  
+
+package UDPPeerToPeer;
+
+  
+/**    
+*     
+* 项目名称：PeerToPeer    
+* 类名称：ServerXml    
+* 类描述：    
+* 创建人：jinyu    
+* 创建时间：2017年2月8日 上午2:15:28    
+* 修改人：jinyu    
+* 修改时间：2017年2月8日 上午2:15:28    
+* 修改备注：    
+* @version     
+*     
+*/
+public class ServerXml {
+public String name;
+public String address;
+public String info;
+}
